4c28637a95f46a558404c42d495b4af6a6eb6c19
mukesh9451 <160834227+mukesh9451@users.noreply.github.com>
2026-05-02 03:06:43 +0530

    Update DeliveryOptions.jsx

--- ecommerce-project/src/pages/ckeckout/DeliveryOptions.jsx
+++ ecommerce-project/src/pages/ckeckout/DeliveryOptions.jsx
@@ -1,45 +1,73 @@
 import axios from "axios";
 import { formateMoney } from "../../utils/money";
-import  dayjs  from "dayjs";
-export function DeliveryOptions({deliveryOptions,cartItem,loadCart}){
+import dayjs from "dayjs";
 
-    return(
-        <div  className="delivery-options">
-            <div className="delivery-options-title">
-                Choose a delivery option:
-            </div>
-        {deliveryOptions.map((deliveryOption)=>{
-                  let priceString="FREE - Shipping";
-        
-        if(deliveryOption.priceCents>0){
-                priceString=`${formateMoney(deliveryOption.priceCents)} - Shipping`;
-            }
-        const updateDeliveryOption= async()=>{
-            await axios.put(`/api/cart-items/${cartItem.productId}`,{
-                deliveryOptionId:deliveryOption._id
-        });
-        await loadCart();
+// ✅ backend URL
+const BASE_URL = "https://scatch-sd9g.onrender.com";
+
+export function DeliveryOptions({ deliveryOptions, cartItem, loadCart }) {
+
+  return (
+    <div className="delivery-options">
+      <div className="delivery-options-title">
+        Choose a delivery option:
+      </div>
+
+      {deliveryOptions.map((deliveryOption) => {
+
+        let priceString = "FREE - Shipping";
+
+        if (deliveryOption.priceCents > 0) {
+          priceString = `${formateMoney(deliveryOption.priceCents)} - Shipping`;
+        }
+
+        // ✅ FIXED API CALL
+        const updateDeliveryOption = async () => {
+          try {
+            await axios.put(
+              `${BASE_URL}/api/cart-items/${cartItem.productId}`,
+              {
+                deliveryOptionId: deliveryOption._id
+              },
+              {
+                withCredentials: true
+              }
+            );
+
+            await loadCart();
+          } catch (err) {
+            console.error("Update delivery error:", err);
+          }
         };
-        
-         return(
-            <div key={deliveryOption._id} className="delivery-option"
-            onClick={updateDeliveryOption}>
-            <input type="radio" 
-            checked={deliveryOption._id===cartItem.deliveryOptionId}
-            onChange={()=>{}}
-            className="delivery-option-input"
-            name={`delivery-option-${cartItem.productId}`} />
-        <div>
-          <div className="delivery-option-date">
-          {dayjs(deliveryOption.estimatedDeliveryTimeMs).format("dddd, MMMM, D")}
-         </div>
-         <div className="delivery-option-price">
-             {priceString}
+
+        return (
+          <div
+            key={deliveryOption._id}
+            className="delivery-option"
+            onClick={updateDeliveryOption}
+          >
+            <input
+              type="radio"
+              checked={deliveryOption._id === cartItem.deliveryOptionId}
+              onChange={() => {}}
+              className="delivery-option-input"
+              name={`delivery-option-${cartItem.productId}`}
+            />
+
+            <div>
+              <div className="delivery-option-date">
+                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format(
+                  "dddd, MMMM, D"
+                )}
+              </div>
+
+              <div className="delivery-option-price">
+                {priceString}
+              </div>
+            </div>
           </div>
-            </div>
-            </div>
-        )
-     })}
-</div>
-    )
+        );
+      })}
+    </div>
+  );
 }
