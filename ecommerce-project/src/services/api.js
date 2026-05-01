175d8d0bdfc19b9fe50fe7391127fe770eebdf14
mukesh9451 <160834227+mukesh9451@users.noreply.github.com>
2026-05-02 01:03:36 +0530

    Update api.js

--- ecommerce-project/src/services/api.js
+++ ecommerce-project/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://scatch-sd9g.onrender.com/api/auth",
+  baseURL: "https://scatch-sd9g.onrender.com",
   withCredentials: true // 🔥 required for cookies
 });
 
