c70678b8e38c26c10a72b198a3521434208ae0bf
mukesh9451 <160834227+mukesh9451@users.noreply.github.com>
2026-05-02 18:08:07 +0530

    Update CartItemDetails.jsx

--- ecommerce-project/src/pages/ckeckout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/ckeckout/CartItemDetails.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { useState, useEffect } from "react";
 import { formateMoney } from "../../utils/money";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+
   const [product, setProduct] = useState(null);
-  const [quantity, setQuantity] = useState(cartItem.quantity);
   const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
 
   useEffect(() => {
     api.get(`/products/${cartItem.productId}`)
@@ -33,34 +34,58 @@ export function CartItemDetails({ cartItem, loadCart }) {
   if (!product) return <p>Loading product...</p>;
 
   return (
-    <>
+    <div className="cart-item-details-grid">
+
+      {/* IMAGE */}
       <img
         className="product-image"
         src={`https://scatch-sd9g.onrender.com/${product.image}`}
         alt={product.name}
       />
 
+      {/* DETAILS */}
       <div className="cart-item-details">
-        <div className="product-name">{product.name}</div>
+
+        <div className="product-name limit-text-to-2-lines">
+          {product.name}
+        </div>
+
         <div className="product-price">
           {formateMoney(product.priceCents)}
         </div>
 
         <div className="product-quantity">
-          Quantity:
-          {isUpdatingQuantity ? (
-            <input
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-          ) : (
-            <span>{cartItem.quantity}</span>
-          )}
+          <span>
+            Quantity:
+            {isUpdatingQuantity ? (
+              <input
+                className="quantity-textbox"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+            ) : (
+              <span className="quantity-label">
+                {cartItem.quantity}
+              </span>
+            )}
+          </span>
 
-          <span onClick={updateQuantity}>Update</span>
-          <span onClick={deleteCartItem}>Delete</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateQuantity}
+          >
+            Update
+          </span>
+
+          <span
+            className="delete-quantity-link link-primary"
+            onClick={deleteCartItem}
+          >
+            Delete
+          </span>
         </div>
+
       </div>
-    </>
+    </div>
   );
 }
