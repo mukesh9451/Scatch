d7de5f25c26db197b0d295dcefddd73999e4c27c
mukesh9451 <160834227+mukesh9451@users.noreply.github.com>
2026-05-02 18:01:27 +0530

    Refactor PaymentSummary component for better structure

--- ecommerce-project/src/pages/ckeckout/PaymentSummary.jsx
+++ ecommerce-project/src/pages/ckeckout/PaymentSummary.jsx
@@ -5,24 +5,65 @@ import { formateMoney } from "../../utils/money";
 export function PaymentSummary({ paymentSummary, loadCart }) {
   const navigate = useNavigate();
 
-  if (!paymentSummary) return <p>Loading payment...</p>;
-
   const createOrder = async () => {
     await api.post("/orders");
     await loadCart();
     navigate("/orders");
   };
 
+  if (!paymentSummary) {
+    return <p>Loading payment...</p>;
+  }
+
   return (
-    <div>
-      <h3>Payment Summary</h3>
+    <div className="payment-summary">
 
-      <p>Items: {paymentSummary.totalItems}</p>
-      <p>Total: {formateMoney(paymentSummary.totalCostCents)}</p>
+      <div className="payment-summary-title">
+        Payment Summary
+      </div>
 
-      <button onClick={createOrder}>
-        Place Order
+      <div className="payment-summary-row">
+        <div>Items ({paymentSummary.totalItems}):</div>
+        <div className="payment-summary-money">
+          {formateMoney(paymentSummary.productCostCents)}
+        </div>
+      </div>
+
+      <div className="payment-summary-row">
+        <div>Shipping & handling:</div>
+        <div className="payment-summary-money">
+          {formateMoney(paymentSummary.shippingCostCents)}
+        </div>
+      </div>
+
+      <div className="payment-summary-row subtotal-row">
+        <div>Total before tax:</div>
+        <div className="payment-summary-money">
+          {formateMoney(paymentSummary.totalCostBeforeTaxCents)}
+        </div>
+      </div>
+
+      <div className="payment-summary-row">
+        <div>Estimated tax (10%):</div>
+        <div className="payment-summary-money">
+          {formateMoney(paymentSummary.taxCents)}
+        </div>
+      </div>
+
+      <div className="payment-summary-row total-row">
+        <div>Order total:</div>
+        <div className="payment-summary-money">
+          {formateMoney(paymentSummary.totalCostCents)}
+        </div>
+      </div>
+
+      <button
+        className="place-order-button button-primary"
+        onClick={createOrder}
+      >
+        Place your order
       </button>
+
     </div>
   );
 }
