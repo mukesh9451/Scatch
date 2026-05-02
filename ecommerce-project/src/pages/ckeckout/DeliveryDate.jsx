4c7d4e9978b8064581e1e9237da1001f7a8b14da
mukesh9451 <160834227+mukesh9451@users.noreply.github.com>
2026-05-02 17:20:57 +0530

    Refactor DeliveryDate component to DeliveryOptions

--- ecommerce-project/src/pages/ckeckout/DeliveryDate.jsx
+++ ecommerce-project/src/pages/ckeckout/DeliveryDate.jsx
@@ -1,19 +1,57 @@
+import axios from "axios";
+import { formateMoney } from "../../utils/money";
 import dayjs from "dayjs";
 
-export function DeliveryDate({ cartItem, deliveryOptions }) {
-  const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
-    return deliveryOption._id === cartItem.deliveryOptionId;
-  });
+const BASE_URL = "https://scatch-sd9g.onrender.com";
 
-  // Removed loadCart() completely - this was causing the loop
-
-  if (!selectedDeliveryOption) {
-    return <div className="delivery-date">Loading delivery date...</div>;
-  }
+export function DeliveryOptions({ deliveryOptions, cartItem, loadCart }) {
 
   return (
-    <div className="delivery-date">
-      Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format("dddd, MMMM, D")}
+    <div className="delivery-options">
+      <div className="delivery-options-title">
+        Choose a delivery option:
+      </div>
+
+      {deliveryOptions.map((deliveryOption) => {
+
+        let priceString = "FREE - Shipping";
+        if (deliveryOption.priceCents > 0) {
+          priceString = `${formateMoney(deliveryOption.priceCents)} - Shipping`;
+        }
+
+        const updateDeliveryOption = async () => {
+          await axios.put(
+            `${BASE_URL}/api/cart-items/${cartItem.productId}`,
+            { deliveryOptionId: deliveryOption._id },
+            { withCredentials: true }
+          );
+          await loadCart();
+        };
+
+        return (
+          <div
+            key={deliveryOption._id}
+            className="delivery-option"
+            onClick={updateDeliveryOption}
+          >
+            <input
+              type="radio"
+              checked={deliveryOption._id === cartItem.deliveryOptionId}
+              readOnly
+            />
+
+            <div>
+              <div className="delivery-option-date">
+                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format("dddd, MMMM, D")}
+              </div>
+
+              <div className="delivery-option-price">
+                {priceString}
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
